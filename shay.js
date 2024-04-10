25cedb3771e5a87a8f5b7e303bddeee649fd07ed
Legacy <highgateshay@yahoo.com>
2024-04-10 18:11:57 -0500

    Added first function

--- shay.js
+++ shay.js
@@ -1,1 +1,12 @@
-console.log("We are back in bussiness")
+function getComputerChoice() {
+    const result = Math.floor(Math.random() * 3) + 1;
+    if (result === 1) {
+        console.log("Rock")
+    }else if (result === 2) {
+        console.log("Papper")
+    }else if (result === 3) {
+        console.log("Scissors")
+    }
+}
+
+console.log(getComputerChoice())
