ca3998753628b267a9912b40392bdcb12980da9a
Legacy <highgateshay@yahoo.com>
2024-04-10 20:39:20 -0500

    Edit Computer function

--- shay.js
+++ shay.js
@@ -9,4 +9,32 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice())
+function playerChoice() {
+    let human = prompt("Select: Rock, Papper, or Scissors");8
+    return human;
+    
+}
+
+function playRound(com, player) {
+     com = getComputerChoice()
+     player = playerChoice()
+    
+     if (com === 1 && player === "Papper") {
+        console.log("Papper beats Rock: Player Wins!!")
+     }else if (player === "Rock" && com === 2) {
+        console.log("Papper beats Rock: Computer Wins!!")
+     }else if (com === 2 && player === "Scissors" ) {
+        console.log("Scissors beats Papper: Player Wins!!")
+     }else if (player === "Papper" && com === 3) {
+        console.log("Scissors beats Papper: Computer Wins!!")
+     }else if (com === 3 && player === "Rock") {
+        console.log("Rock beats Scissors: Player Wins!!")
+     }else if (player === "Scissors" && com === 1) {
+        console.log("Rock beats Scissors: Computer Wins!!")
+     }else{
+        console.log("It's a Tie!!")
+     }
+}
+
+
+console.log(playRound())
