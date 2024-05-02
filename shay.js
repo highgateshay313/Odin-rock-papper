4e085415018083eb790041379d36c8072ba86ed3
Legacy <highgateshay@yahoo.com>
2024-05-02 16:26:47 -0500

    Add rounds

--- shay.js
+++ shay.js
@@ -7,12 +7,15 @@ const playerPoints = document.querySelector("#playerPoints")
 const computerPoints = document.querySelector("#comPoints")
 const playerResult = document.querySelector(".first")
 const computerResults = document.querySelector(".second")
+const round = document.querySelector("#roundNum")
 
 
 
-let choice = ""
+let playerChoice = ""
+let computerChoice = ""
 let playerScore = 0
 let computerScore = 0
+let rounds = 1
 
 let currentChoice = 0
 
@@ -22,26 +25,30 @@ let gameChoices = gameChoice
 
 function playGame() {
    let shuffelId = setInterval(computerShuffel, 150)
+
    
    playerSelection.forEach(Option => {
       Option.addEventListener("click", (e) => {
-         
+         round.textContent = rounds++
+
+      
          clearInterval(shuffelId)
          
-         choice = e.target.innerHTML
-         playerResult.append(choice)
-         playerResult.innerHTML = choice
-         
+         playerChoice = e.target.textContent
+         // playerResult.append(playerChoice)
+         playerResult.textContent = playerChoice
+
+         gameWinner()
          
         
       })
-      gameWinner()
+      
    })
 
    function computerShuffel() {
-      gameChoices = gameChoice[currentChoice]
-      computerResults.append(gameChoices)
-      computerResults.innerHTML = gameChoices
+      computerChoice = gameChoice[currentChoice]
+      // computerResults.append(gameChoices)
+      computerResults.textContent = computerChoice
 
       if (currentChoice < gameChoice.length -1) {
          currentChoice++;
@@ -54,18 +61,33 @@ function playGame() {
    function gameWinner() {
       const theWinner = document.querySelector(".winnerText")
       let champion = ""
-      theWinner.append(champion)
-         theWinner.innerHTML = champion
+      // theWinner.append(champion)
+         
 
-      if (playerResult == computerResults) {
+      if (playerChoice === computerChoice) {
          champion = "It's a Tie"
+      }else if (playerChoice === "Rock" && computerChoice === "scissors") {
+         champion = "Player Wins"
+         playerScore++
+      }else if (playerChoice === "Paper" && computerChoice === "Rock") {
+         champion = "Player Wins"
+         playerScore++
+      }else if (playerChoice === "Scissors" && computerChoice === "Paper") {
+         champion = "Player Wins"
+         playerScore++
       }else{
-         champion = "It works"
+         champion = "Computer Wins"
+         computerScore++
       }
+      playerPoints.textContent = playerScore
+      computerPoints.textContent= computerScore
+      theWinner.textContent = champion;
    }
+
+ 
    
 }
-playGame()
+playGame(5)
 
 
 
